368e221e3612357e08b485b170b4713a5d03307f
Orrin Ahola <orrin.ahola@gmail.com>
2021-07-11 12:12:06 -0400

    Add files via upload

--- calc/abcdCalc/index.js
+++ calc/abcdCalc/index.js
@@ -1,12 +1,24 @@
 
 
 function calcABCD() {
-let abcd
-abcd =  Number(document.querySelector('input[name="age"]:checked').value) +
-        Number(document.querySelector('input[name="bp"]:checked').value) +
-        Number(document.querySelector('input[name="clinical"]:checked').value) +   
-        Number(document.querySelector('input[name="duration"]:checked').value) +
-        Number(document.querySelector('input[name="dm"]:checked').value) 
+    let abcd;
+    let risk;
 
-document.getElementById('abcd').innerText = "ABCD2 = " + abcd;
+    abcd =  Number(document.querySelector('input[name="age"]:checked').value) +
+            Number(document.querySelector('input[name="bp"]:checked').value) +
+            Number(document.querySelector('input[name="clinical"]:checked').value) +   
+            Number(document.querySelector('input[name="duration"]:checked').value) +
+            Number(document.querySelector('input[name="dm"]:checked').value) 
+
+    if (abcd < 4) {
+        risk = " (low)"
+    } else {
+        if (abcd > 5) {
+            risk = " (**high**)"
+        } else {
+            risk = " (**medium**)"
+        }
+    }
+
+    document.getElementById('abcd').innerText = "ABCD2 = " + abcd + risk;
 }
